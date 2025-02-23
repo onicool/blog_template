d1710c56ab65260b30159c9a1014364a267f04b3
onicool <akr.oshd@gmail.com>
2025-02-23 11:32:54 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel'
 
 export default defineConfig({
   site: 'https://www.nameless-traders-note.site/',
